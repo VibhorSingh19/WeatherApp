94c463b2e1d8545d177a48ea4527ad8ef395a6ef
Vibhor Singh <vibhor.formal@gmail.com>
2021-02-01 08:47:58 +0530

    Updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 var requests = require("requests");
 
 
-const homeFile = fs.readFileSync("home.html", "utf-8");
+const homeFile = fs.readFileSync("index.html", "utf-8");
 
 
 const replaceVal = (tempVal, orgVal) => {
